453796f2369afd99da8772bfeab1ffc1987181b4
Smarandii <olegsmarandi@gmail.com>
2025-04-25 22:27:16 +0300

    nakama_ideas_manager | v0.4.5 | Bug fix

--- nn_ideas_manager/core/db/config.py
+++ nn_ideas_manager/core/db/config.py
@@ -1,4 +1,5 @@
 from os import getenv
 
-DB_DSN = getenv("DATABASE_DSN", "postgres://postgres:postgres@localhost:5432/postgres")
+RESERVE_DB_DSN = getenv("RESERVE_DB_DSN", "postgres://postgres:postgres@localhost:5432/postgres")
+DB_DSN = getenv("DATABASE_DSN", "postgres://postgres:postgres@db:5432/postgres")
 POOL_MIN, POOL_MAX = 1, 10
--- nn_ideas_manager/core/db/connection.py
+++ nn_ideas_manager/core/db/connection.py
@@ -1,15 +1,26 @@
 import asyncpg
-from nn_ideas_manager.core.db.config import DB_DSN, POOL_MIN, POOL_MAX
+from nn_ideas_manager.core.db.config import DB_DSN, RESERVE_DB_DSN, POOL_MIN, POOL_MAX
 
 _pool: asyncpg.Pool | None = None
 
 
 async def get_pool() -> asyncpg.Pool:
     global _pool
-    if _pool is None:
-        _pool = await asyncpg.create_pool(
-            dsn=DB_DSN,
-            min_size=POOL_MIN,
-            max_size=POOL_MAX,
-        )
-    return _pool
+    try:
+        if _pool is None:
+            _pool = await asyncpg.create_pool(
+                dsn=DB_DSN,
+                min_size=POOL_MIN,
+                max_size=POOL_MAX,
+            )
+
+        return _pool
+    except Exception:
+        if _pool is None:
+            _pool = await asyncpg.create_pool(
+                dsn=RESERVE_DB_DSN,
+                min_size=POOL_MIN,
+                max_size=POOL_MAX,
+            )
+
+        return _pool
